236e2287c2b37cac791ee97141f5f97fc8242ee9
rj <rajesh@mylocalbroker.com.au>
2016-06-04 09:27:35 +1000

    init

---
+++ app/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+-- comment
+import Lib
+import Data.List
+import System.IO
+
+
+--INT -2^63  2^63
+minInt = minBound :: Int
+maxInt = maxBound :: Int
+
+main :: IO ()
+main = someFunc
